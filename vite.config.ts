268df08370b4f8840bfee3e739f471737ebde3de
Sravani <sravanivemuri45@gmail.com>
2026-06-08 19:08:23 +0530

    Configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/bestfriend/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
